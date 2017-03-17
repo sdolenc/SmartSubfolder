afa63fc2d83c08cea6bd43e38f5948b15f49b86d
Stephen Dolenc <stepdo@microsoft.com>
2017-03-16 18:23:25 -0700

    experimental change

--- ssf.py
+++ ssf.py
@@ -2,12 +2,16 @@
 #!/usr/bin/python
 
 import os
+import sys
 import time
 import shutil
 
-#todo: take directory as an argument. import
-#   getopt or argparse, sys also has argv
-directory = "C:\\max\\import\\combined"
+# take directory as an argument.
+# future: use getopt or argparse
+if (len(sys.argv) != 2):
+    exit(3)
+directory = sys.argv[1] # "C:\\max\\import\\combined"
+#todo: verify a) exists b) directory and c) doesn't end w/ slash
 
 backup = directory + '_bak'
 images = [ ".jpg", ".png" ]
@@ -15,15 +19,53 @@ videos = [ ".mp4", ".webm", ".mov" ]
 
 # CLASSES
 
+#todo:init,adds
+#todo:drain
 class Directories:
-    'Base Directory and list of pending subfolders'
+    'Base Directory and collection of pending subfolders'
     baseDirectory = ''
-    subFolders = [] # Subdirectory object
+    subFolders = dict() # Subdirectory object
 
+    def __init__(self, basePath):
+        self.baseDirectory = basePath
+
+    def addFile(self, folder, file):
+        # Ensure Folder Exists
+        if (self.subFolders.get(folder) == None):
+            subDir = Subdirectory(folder, file)
+            self.subFolders[folder] = subDir
+        else:
+            # Add file
+            self.subFolders[folder].files.append(file)
+
+    def moveFiles(self):
+        #print(self.subFolders["imgs"].files)
+        #print(self.subFolders["vids"].files)
+        print(self.subFolders)
+        for a in self.subFolders:
+            print(a)
+            print()
+        print(self.subFolders.items())
+        for b in self.subFolders.items():
+            print(b)
+            print()
+        exit()
+        print(self.subFolders.values)
+        if (len(self.subFolders) > 1):
+            for sf, value in self.subFolders.items():
+                print(str(sf))
+                for f in sf.files:
+                    moveFile(self.baseDirectory, sf.folderName, f)
+
+#todo: consider making class act like a dictionary
 class Subdirectory:
     'Folder and list of pending files'
     folderName = ''
     files = [] # fileName strings
+
+    def __init__(self, folder, file):
+        #self.folderName = folder
+        self.files.append(file)
 
 # HELPERS
 
@@ -74,7 +116,8 @@ def makeBackup():
     except:
         printStatus('errors', action)
         # We don't want to proceed if this fails.
-        raise
+        #raise
+        pass
     printStatus('post', action)
 
 # todo: conditionalize when the dates are different, recurse to subdirs
@@ -86,20 +129,25 @@ def directoryByDate():
 
 #todo: conditionalize when the types are different, recurse to subdirs
 def directoryByType():
+    pendingMoves = Directories(directory)
     for file in os.listdir(directory):
+        #todo: can this return subfolders? if so, conditionalize to files
         if hasEnding(file, images):
-            moveFile(directory, "imgs", file)
+            #moveFile(directory, "imgs", file)
+            pendingMoves.addFile("imgs", file)
         elif hasEnding(file, videos):
-            moveFile(directory, "vids", file)
+            #moveFile(directory, "vids", file)
+            pendingMoves.addFile("vids", file)
+    pendingMoves.moveFiles()
 
 # EXECUTION
 
 makeBackup()
 
-directoryByDate()
+#directoryByDate()
 
 #todo: reactivate after above todos
-#directoryByType()
+directoryByType()
 
 #todo: refactor into functions
 #todo: verify file count and total size equals backup after completion
